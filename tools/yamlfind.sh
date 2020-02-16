22b98a8d21dba9d47d7f15d390abc04f215eaeeb
Carlos Camacho <ccamacho@redhat.com>
2020-02-16 06:34:07 -0500

    lint cleanup

---
+++ tools/yamlfind.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+count=`find . -not -wholename "*/node_modules/*" -and -not -wholename "*.tox/*" -and -name "*.yaml" | wc -l`
+if [ "$count" != "0" ]; then
+    echo "yaml extension not allowed"
+    exit 1
+else
+    echo "no yaml found"
+fi
