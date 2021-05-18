29af246f6e013c99721c55ef81e39f921d5ed011
Glenn Marcy <homelab@glenn4u.com>
2021-05-18 19:30:06 -0400

    feat: normalize inventory groups and cleanup
    This commit remove the distro prefix from the group names in
    the inventory files so that the names are consistent and are
    common across distros.  Additional cleanup to refactor tasks
    like systemd user services to be shared across service roles,
    moving certificate info to common config and moving tasks to
    check that nodes are up into the tasks which create them.

--- ci/run_kubeinit.sh
+++ ci/run_kubeinit.sh
@@ -205,7 +205,7 @@ if [[ "$SCENARIO" == "submariner" ]]; then
         -v -i ./hosts/okd/inventory \
         --become \
         --become-user root \
-        -e kubeinit_libvirt_multicluster_dns_forward_enabled=True \
+        -e kubeinit_bind_multicluster_dns_forward_enabled=True \
         -e @scenario_variables.yml \
         ./playbooks/okd.yml
 
@@ -215,7 +215,7 @@ if [[ "$SCENARIO" == "submariner" ]]; then
         -v -i ./hosts/rke/inventory \
         --become \
         --become-user root \
-        -e kubeinit_libvirt_multicluster_dns_forward_enabled=True \
+        -e kubeinit_bind_multicluster_dns_forward_enabled=True \
         -e kubeinit_libvirt_multicluster_keep_predefined_networks=True \
         -e @scenario_variables.yml \
         ./playbooks/rke.yml
