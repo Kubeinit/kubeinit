74c666508a09dc420252152bfd4443ef7f39ee74
Carlos Camacho <carloscamachoucv@gmail.com>
2021-08-05 12:35:16 +0200

    ci: balance CI cluster load
    This commit balances the nodes
    in the 3 nodes cluster so it
    have enough resources.

--- ci/run_e2e.sh
+++ ci/run_e2e.sh
@@ -205,12 +205,12 @@ if [[ "$HYPERVISORS" == "3" ]]; then
     #
     # Controllers
     find ./hosts/ -type f -exec sed -i -E -e "/.*-controller-01 ansible_host/ s/hypervisor-01/hypervisor-01/g"  {} \;
-    find ./hosts/ -type f -exec sed -i -E -e "/.*-controller-02 ansible_host/ s/hypervisor-01/hypervisor-02/g" {} \;
-    find ./hosts/ -type f -exec sed -i -E -e "/.*-controller-03 ansible_host/ s/hypervisor-01/hypervisor-03/g" {} \;
+    find ./hosts/ -type f -exec sed -i -E -e "/.*-controller-02 ansible_host/ s/hypervisor-01/hypervisor-01/g" {} \;
+    find ./hosts/ -type f -exec sed -i -E -e "/.*-controller-03 ansible_host/ s/hypervisor-01/hypervisor-02/g" {} \;
 
     # Computes
     find ./hosts/ -type f -exec sed -i -E -e "/.*-compute-01 ansible_host/ s/hypervisor-01/hypervisor-01/g" {} \;
-    find ./hosts/ -type f -exec sed -i -E -e "/.*-compute-02 ansible_host/ s/hypervisor-01/hypervisor-02/g" {} \;
+    find ./hosts/ -type f -exec sed -i -E -e "/.*-compute-02 ansible_host/ s/hypervisor-01/hypervisor-03/g" {} \;
 
     # Services
     find ./hosts/ -type f -exec sed -i -E -e "/.*-service-01 ansible_host/ s/hypervisor-01/hypervisor-03/g" {} \;
