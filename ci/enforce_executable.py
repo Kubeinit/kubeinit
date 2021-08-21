1b2d14d7d0806f62b811a08fd697d4dd8d566364
Carlos Camacho <carloscamachoucv@gmail.com>
2021-08-21 15:54:37 +0200

    ci: enforce the executable usage in shell
    This commit enforces the usage of the
    bash interpreter when calling the
    shell module.

---
+++ ci/enforce_executable.py
@@ -0,0 +1,98 @@
+#!/usr/bin/env python
+
+"""
+Copyright kubeinit contributors.
+
+Licensed under the Apache License, Version 2.0 (the "License"); you may
+not use this file except in compliance with the License. You may obtain
+a copy of the License at:
+
+http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+License for the specific language governing permissions and limitations
+under the License.
+"""
+
+import os
+import sys
+from pathlib import Path
+
+from ansible.errors import AnsibleParserError
+from ansible.inventory.manager import InventoryManager
+from ansible.parsing.dataloader import DataLoader
+from ansible.playbook.task import Task
+from ansible.vars.manager import VariableManager
+
+import ruamel.yaml
+import ruamel.yaml.util
+
+yaml = ruamel.yaml.YAML()
+
+Task._validate_attributes = lambda *args: None
+
+
+def get_tasks(obj):
+    """Get each task."""
+    if isinstance(obj, list):
+        for o in obj:
+            yield from get_tasks(o)
+    else:
+        found = False
+        for key in ('tasks',
+                    'pre_tasks',
+                    'post_tasks',
+                    'handlers',
+                    'block',
+                    'rescue',
+                    'always'):
+            if obj.get(key):
+                found = True
+                yield from get_tasks(obj.get(key))
+        if not found:
+            yield obj
+
+
+def check_tasks(file):
+    """Parse a tasks file."""
+    print(file)
+    errors = []
+    with open(file) as f:
+        data = yaml.load(f)
+
+    # If data is None it means is not a valid YAML file
+    if data:
+        for ds in get_tasks(data):
+            try:
+                task = Task.load(ds,
+                                 variable_manager=variable_manager,
+                                 loader=loader)
+                if task.action == 'ansible.builtin.shell':
+                    if 'executable' not in task._attributes['args']:
+                        errors.append((file, task.action, "must define the executable as an args, like /bin/bash"))
+            except AnsibleParserError:
+                pass
+            except Exception:
+                pass
+    return errors
+
+
+loader = DataLoader()
+inventory = InventoryManager(loader=loader, sources='localhost,')
+variable_manager = VariableManager(loader=loader, inventory=inventory)
+
+dirname = os.path.abspath(os.path.dirname(__file__))
+search_folder = os.path.join(dirname, '../kubeinit/roles/')
+
+to_fix = []
+
+for path in Path(search_folder).rglob('*.yml'):
+    to_fix = to_fix + check_tasks(path)
+
+if len(to_fix) > 0:
+    msg = 'If we use the shell module we must set the executable to /bin/bash:'
+    print(msg)
+    print(str(to_fix))
+    sys.exit(1)
