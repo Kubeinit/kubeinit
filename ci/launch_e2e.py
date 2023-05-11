04382bd35d5636b19d4103d11abe58a8991afaf9
Carlos Camacho <carloscamachoucv@gmail.com>
2023-05-11 10:48:16 +0200

    ci: update GH tracker id
    This commit updates the GH CI results
    tracker id.

--- ci/launch_e2e.py
+++ ci/launch_e2e.py
@@ -37,6 +37,8 @@ from pybadges import badge
 
 GH_LABELS = []
 
+GH_RES_TRACKER = 744
+
 
 def main(job_type, cluster_type, job_label, pr_id, verbosity, nolog=False):
     """Run the main method."""
@@ -249,7 +251,7 @@ def main(job_type, cluster_type, job_label, pr_id, verbosity, nolog=False):
                 else:
                     state = "failure"
                 dur_mins = str(round((time.time() - start_time) / 60, 2))
-                issue = repo.get_issue(number=698)
+                issue = repo.get_issue(number=GH_RES_TRACKER)
                 issue.create_comment(body="%s-%s-%s-%s-%s-%s-%s-%s-%s" % (distro,
                                                                           driver,
                                                                           masters,
